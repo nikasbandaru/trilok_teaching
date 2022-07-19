10e6f51feb3b103566b001adeea12f9ef5fa17de
nikasbandaru <37647584+nikasbandaru@users.noreply.github.com>
2022-07-19 12:58:46 +0530

    Add files via upload

---
+++ basic_conditional.java
@@ -0,0 +1,31 @@
+// import java.util.*;
+public class basic_conditional{
+    public static void main(String[] args) {
+        /*
+         * if else: it runs block of code based on a condition
+         * 
+         */
+        int num=11;
+       /* 
+       / -> quotient
+     % -> remainder 
+        5%3 --> quotient 1 (5/3) remainder 2 (5%3)
+        9%5 --> quotient 1 (9/5) remainder 4  (9%4)
+        12%5 --> 2 (12/5) 2 (12%5)
+
+         */
+        //to  check a number is even or odd 
+   
+       if(num%2==0) {
+        // int x=5;
+        // x=x+1;
+        System.out.println("even");
+    }
+    else
+    {
+
+        System.out.println("odd");
+    }
+    
+    }
+}
